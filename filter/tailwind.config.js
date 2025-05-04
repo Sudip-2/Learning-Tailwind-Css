d399deab73d15136da5dc8d88dd8b6088aadad9b
Sudip Paul <146758688+Sudip-2@users.noreply.github.com>
2025-05-04 19:38:08 +0530

    made changes

--- filter/tailwind.config.js
+++ filter/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./proj1/index.html","./proj2/index.html","./proj3/index.html"],
+  content: ["./index.html"],
   theme: {
     extend: {},
   },
